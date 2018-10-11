25b938af0d5e8f45a2d3c734dd352084d80a587d
Viktor Speranskyy <vencedor@ukr.net>
2018-10-11 18:05:04 +0300

    Initial commit

---
+++ src/itschool/Main.java
@@ -0,0 +1,11 @@
+package itschool;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    short a = 32767, b = 32767, c = 3;
+	    c = (short) (a + b);
+        System.out.println(c);
+    }
+}
