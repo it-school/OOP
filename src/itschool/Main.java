3fa37e1a906cad4c2b0f798449a01c5bd6a92571
Viktor Speranskyy <vencedor@ukr.net>
2018-10-18 20:16:58 +0300

    Update 2

--- src/itschool/Main.java
+++ src/itschool/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+/*
         final int SIZE = 4;
         int r = 0, c = 0, n = 1, dC = 1, dR = 0;
 
@@ -50,5 +51,60 @@ public class Main {
             System.out.println();
 
         }
+*/
+        final int SIZE = 100;
+        int[] a = new int [SIZE];
+        int[] b = new int [SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            a[i] = (int)(Math.random()*100);
+        }
+
+        System.out.println(Arrays.toString(a));
+
+        Arrays.sort(a);
+
+        System.out.println(Arrays.toString(a));
+/*
+        for (int item : a)
+        {
+            if (item % 2 == 0)
+                System.out.println(item);
+        }
+*/
+
+        int temp;
+        for (int i = 0; i < a.length/2; i++)
+        {
+            temp = a[i];
+            a[i] = a[a.length-1-i];
+            a[a.length-1-i] = temp;
+        }
+
+        System.out.println(Arrays.toString(a));
+
+
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+
+        int n = Arrays.binarySearch(a, 62);
+        System.out.println(n >= 0 ? n : "No such element");
+
+        //b = a;
+        b = Arrays.copyOf(a, SIZE);
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+
+        //b[0] = 100;
+
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+
+        if (a == b)
+            System.out.println("Equal");
+
+        if (Arrays.equals(a, b))
+            System.out.println("Equal");
+
     }
 }
